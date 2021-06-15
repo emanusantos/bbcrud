2709ee537f59cae7b8bdfc5dc1302527ccbd2308
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-15 14:36:33 -0300

    Added styled-components dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import LoginRegister from './Pages/loginRegister';
 
 function App() {
   return (
-    <div className="App">
-      <h1>salve</h1>
-    </div>
+    <LoginRegister />
   );
 }
 
