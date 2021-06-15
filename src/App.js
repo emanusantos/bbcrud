923c1759281f5c432cd5cd01110f51d12c8b7a00
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-15 18:57:19 -0300

    Started to build the homepage and the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 //import Login from './Pages/Login';
-import Register from './Pages/Register';
+//import Register from './Pages/Register';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <Register />
+    <Home />
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <div className="apiInfo">
+                    <li>Characters</li>
+                    <li>Episodes</li>
+                    <li>Quotes</li>
+                </div>
+                <li>My profile</li>
+            </ul>
+        </nav>
+    )
+}
