00c21b7698a7c788134daf2ae19915885b0deba2
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-15 20:35:00 -0300

    Finished Axios register method (need to add the prevent default)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 //import Login from './Pages/Login';
-//import Register from './Pages/Register';
-import Home from './Pages/Home';
+import Register from './Pages/Register';
+//import Home from './Pages/Home';
 
 function App() {
   return (
-    <Home />
+    <Register />
   );
 }
 
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Container, Input, InputArea, Form } from '../Components/helper.js';
+import Axios from 'axios';
 
 export default function Login() {
 
@@ -8,9 +9,23 @@ export default function Login() {
     const [usernameReg, setUsernameReg] = useState('');
     const [passwordReg, setPasswordReg] = useState('');
 
+    const addUser = async () => {
+        try {
+            await Axios.post('http://localhost:3001/create', {
+                name: name,
+                email: email,
+                username: usernameReg,
+                password: passwordReg
+            });
+            console.log('Success')
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <Container padding={'6em'}>
-            <Form padding ={'4em'} action="">
+            <Form padding ={'4em'}>
             <h4>Name</h4>
                 <InputArea>
                     <Input type="text" onChange={event => {setName(event.target.value)}} />
@@ -27,7 +42,7 @@ export default function Login() {
                 <InputArea>
                     <Input type="password" onChange={event => {setPasswordReg(event.target.value)}} />
                 </InputArea>
-                <Button>Register</Button>
+                <Button type="button" onClick={addUser}>Register</Button>
             </Form>
         </Container>
     )
