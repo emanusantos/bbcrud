488407c036ea6d9e480a0d7f8a6c5622c7810018
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-17 13:55:21 -0300

    Made changes to the Characters page

--- src/Pages/Characters.js
+++ src/Pages/Characters.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar } from '../Components/helper.js';
 
 export default function Characters() {
+
+    const fetchItems = async () => {
+        const data = await fetch('https://www.breakingbadapi.com/api/characters?limit=10&offset=10?category=Breaking+Bad');
+        const characters = await data.json();
+        setCharacter(characters);
+        console.log(character);
+    };
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+    const [character, setCharacter] = useState([])
+
+
     return (
         <>
             <Navbar />
             <h1>Oi</h1>
             <h1>Oi</h1>
+            <ul>
+                {character.map(item => (
+                <li>
+                    <img src={`${item.img}`} alt="" />
+                    {item.name}
+                </li>
+                ))}
+            </ul>
         </>
     )
 }
