52c55d3ac0f66c8d428677dfa03ba7bc9ebcdde4
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-06-17 12:47:58 -0300

    Started the Characters page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 //import Login from './Pages/Login';
-import Register from './Pages/Register';
+//import Register from './Pages/Register';
 //import Home from './Pages/Home';
+import Characters from './Pages/Characters';
 
 function App() {
   return (
-    <Register />
+    <Characters />
   );
 }
 
---
+++ src/Pages/Characters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from '../Components/helper.js';
+
+export default function Characters() {
+    return (
+        <>
+            <Navbar />
+            <h1>Oi</h1>
+            <h1>Oi</h1>
+        </>
+    )
+}
